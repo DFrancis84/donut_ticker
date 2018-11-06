7f96a2e8fc3e5dce2fc6772df492b9229844ce8f
Devin Francis <devin.francis@eltoro.com>
2018-11-06 15:34:31 -0500

    added confetti to screen.  currently works in codepen, but not in actual html.  need to see why the reset button is turned off when establishing the canvas. currently have canvas commented out so original functionality is in tact.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 var countDownDate = localStorage.getItem('startDate');
 if (countDownDate) {
     countDownDate = new Date(countDownDate);
@@ -31,6 +30,69 @@ var x = setInterval(function() {
 
 // Reset button to reset the timer
 document.getElementById("btn").addEventListener("click", function(){
+    // Reset Timer
     countDownDate = new Date();
+    // Update localstorage so timer doesn't reset upon reload
     localStorage.setItem('startDate', countDownDate);
+    // Confetti fall from top of page to bottom (Need to set a timer so it will not run non-stop)
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var W = window.innerWidth;
+    var H = window.innerHeight;
+    canvas.width = W;
+    canvas.height = H;
+  
+    var mp = 1000; //max particles
+    var particles = [];
+    for (var i = 0; i < mp; i++) {
+      particles.push({
+        x: Math.random() * W, //x-coordinate
+        y: Math.random() * H, //y-coordinate
+        r: Math.random() * 18 + 1, //radius
+        d: Math.random() * mp, //density
+        color: "rgba(" + Math.floor((Math.random() * 255)) + ", " + Math.floor((Math.random() * 255)) + ", " + Math.floor((Math.random() * 255)) + ", 0.8)",
+        tilt: Math.floor(Math.random() * 5) - 5
+      });
+    }
+  
+    //Lets draw the flakes
+    function draw() {
+      ctx.clearRect(0, 0, W, H);
+      for (var i = 0; i < mp; i++) {
+        var p = particles[i];
+        ctx.beginPath();
+        ctx.lineWidth = p.r;
+        ctx.strokeStyle = p.color; // Green path
+        ctx.moveTo(p.x, p.y);
+        ctx.lineTo(p.x + p.tilt + p.r / 2, p.y + p.tilt);
+        ctx.stroke(); // Draw it
+      }
+  
+      update();
+    }
+  
+    var angle = 0;
+  
+    function update() {
+      angle += 0.01;
+      for (var i = 0; i < mp; i++) {
+        var p = particles[i];
+        p.y += Math.cos(angle + p.d) + 1 + p.r / 2;
+        p.x += Math.sin(angle) * 2;
+        if (p.x > W + 5 || p.x < -5 || p.y > H) {
+          if (i % 3 > 0) //66.67% of the flakes
+          {
+            particles[i] = {
+              x: Math.random() * W,
+              y: -10,
+              r: p.r,
+              d: p.d,
+              color: p.color,
+              tilt: p.tilt
+            };
+          }
+        }
+      }
+    }
+    setInterval(draw, 20);
 });
